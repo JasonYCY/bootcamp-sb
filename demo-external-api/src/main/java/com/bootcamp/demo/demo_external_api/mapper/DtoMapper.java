c3e71f7ca11221075a3714938bfdc0b71b7f6a6e
Jason Yau <jasonyau0130@gmail.com>
2026-01-21 15:06:11 +0800

    format mapper method

--- demo-external-api/src/main/java/com/bootcamp/demo/demo_external_api/mapper/DtoMapper.java
+++ demo-external-api/src/main/java/com/bootcamp/demo/demo_external_api/mapper/DtoMapper.java
@@ -5,6 +5,10 @@ import com.bootcamp.demo.demo_external_api.model.dto.UserDTO;
 
 public class DtoMapper {
   public static UserDto map(UserDTO userDTO) {
-    return new UserDto(userDTO.getId(), userDTO.getName(), userDTO.getUsername(), userDTO.getEmail());
+    return new UserDto(
+      userDTO.getId(), 
+      userDTO.getName(), 
+      userDTO.getUsername(), 
+      userDTO.getEmail());
   }
 }
