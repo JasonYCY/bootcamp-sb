adb4aa066a9aa17f6326b252f47ba801ab93a5ae
Jason Yau <jasonyau0130@gmail.com>
2026-01-26 17:13:06 +0800

    Handle RestClientException when failed to get data from hk observatory api

--- demo-weather/src/main/java/com/bootcamp/demo/demo_weather/exception/GlobalExceptionHandler.java
+++ demo-weather/src/main/java/com/bootcamp/demo/demo_weather/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.bootcamp.demo.demo_weather.exception;
 
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.RestClientException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -10,6 +11,14 @@ public class GlobalExceptionHandler {
     return ExceptionDTO.builder()
       .code(SysException.PARAM_NOT_MATCH.getCode())
       .message(SysException.PARAM_NOT_MATCH.getMessage() + " " + e.getMessage())
+      .build();
+  }
+
+  @ExceptionHandler(value = RestClientException.class)
+  public ExceptionDTO handle(RestClientException e) {
+    return ExceptionDTO.builder()
+      .code(SysException.GET_REQUEST_FAIL.getCode())
+      .message(SysException.GET_REQUEST_FAIL.getMessage() + " " + e.getMessage())
       .build();
   }
 
--- demo-weather/src/main/java/com/bootcamp/demo/demo_weather/exception/SysException.java
+++ demo-weather/src/main/java/com/bootcamp/demo/demo_weather/exception/SysException.java
@@ -4,7 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum SysException {
-  PARAM_NOT_MATCH(90000, "Parameters not match.");
+  PARAM_NOT_MATCH(90000, "Parameters not match."),
+  GET_REQUEST_FAIL(90001, "Failed to retrieve data from Hong Kong Observatory API");
 
   private int code;
   private String message;
