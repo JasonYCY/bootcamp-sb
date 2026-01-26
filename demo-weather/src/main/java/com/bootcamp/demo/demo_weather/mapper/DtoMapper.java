2c7d217c99dde2b15d0884bfedce7b4a904a60d9
Jason Yau <jasonyau0130@gmail.com>
2026-01-26 16:23:27 +0800

    remove formatter

--- demo-weather/src/main/java/com/bootcamp/demo/demo_weather/mapper/DtoMapper.java
+++ demo-weather/src/main/java/com/bootcamp/demo/demo_weather/mapper/DtoMapper.java
@@ -1,16 +1,11 @@
 package com.bootcamp.demo.demo_weather.mapper;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import com.bootcamp.demo.demo_weather.dto.WeatherDto;
-import com.bootcamp.demo.demo_weather.mapper.formatter.DateFormatter;
 import com.bootcamp.demo.demo_weather.model.dto.WeatherForecastDTO;
 
 @Component
 public class DtoMapper {
-  @Autowired
-  DateFormatter dateFormatter;
-
   public WeatherDto toWeatherDto(WeatherForecastDTO weatherForecastDTO) {
     return WeatherDto.builder()
       .forecastDate(weatherForecastDTO.getForecastDate())
